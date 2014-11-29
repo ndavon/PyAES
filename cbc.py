900e49a121b0a3b3832645f0209aef1f2973e505
ndavon <nicolai.davidsson@gmail.com>
2014-11-29 22:03:48 +0100

    class definitions

---
+++ cbc.py
@@ -0,0 +1,18 @@
+
+class CBC:
+
+    def decrypt(self, message, key, iv):
+        pass
+
+    def decrypt_file(self, file, key, iv):
+        pass
+
+    def encrypt(self, file, key, iv):
+        pass
+
+    def encrypt_file(self, message, key, iv):
+        pass
+
+class ECB:
+
+    def decrypt(self):
