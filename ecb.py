45155bbd10ea34cf461c02ffa78ffc04ac9055da
ndavon <nicolai.davidsson@gmail.com>
2014-12-02 00:00:47 +0100

    moved tables to tables.py and wrote code to run ecb.py from command line

--- ecb.py
+++ ecb.py
@@ -1,15 +1,9 @@
 import core
-import base64
 import itertools
-import struct
-import sys
-
-testMessage = "Hallo"
-testKey = "0123456789abcdef"
 
 def encrypt(message, key, b64=False):
     # convert message to 128-bit blocks
-    blocks = core.message_to_blocks(map(ord, message))
+    blocks = core.to_blocks(map(ord, message))
 
     # transform each block and apply transformation from array to matrix
     for block in blocks:
@@ -19,37 +13,42 @@ def encrypt(message, key, b64=False):
 
     # flatten out blocks
     flat = list(itertools.chain.from_iterable(blocks))
-    print "Encrypted message:"
-    print flat
-    print_chars(flat)
-    return flat
+    result = ''.join(map(unichr, flat))
+    return result
 
 def encrypt_file(file, key, b64=False):
     with open(file, 'r') as fr:
         message = encrypt(fr.read(), key, b64) # TODO: write
         with open('encrypted', 'w') as fw:
-            fw.write(message)
+            fw.write(message.encode('utf-8'))
 
 def decrypt(message, key, b64=False):
-    blocks = core.message_to_blocks(message)
+    blocks = core.to_blocks(map(ord, message))
     for block in blocks:
         core.transform(block)
         core.decrypt_block(block, key)
         core.transform(block)
 
     flat = list(itertools.chain.from_iterable(blocks))
-    print "Decrypted message:"
-    print flat
-    print_chars(flat)
-    return flat
-
-def print_chars(list):
-    for i in range(0,len(list)):
-        sys.stdout.write(unichr(list[i])),
-    print ""
+    result = ''.join(map(unichr, flat))
+    return result
 
 def decrypt_file(file, key, b64=False):
     with open(file, 'r') as fr:
-        message = decrypt(fr.read(), key, b64) # TODO: write
-        with open('encrypted', 'w') as fw:
-            fw.write(message)
+        message = decrypt(fr.read().decode('utf-8'), key, b64) # TODO: write
+        with open('decrypted', 'w') as fw:
+            fw.write(message.encode('utf-8'))
+
+if __name__ == "__main__":
+    message = "Hallo"
+    key = "0123456789abcdef"
+
+    encrypted = encrypt(message, key)
+    decrypted = decrypt(encrypted, key)
+
+    print 'Message: ', message
+    print 'Encrypted: ', encrypted
+    print 'Decrypted: ', decrypted
+
+    encrypt_file('testfile', key)
+    decrypt_file('encrypted', key)
