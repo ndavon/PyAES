e815c3b7aa69aca52155960d9ef616e1dd60a222
ndavon <nicolai.davidsson@gmail.com>
2014-12-01 18:19:10 +0100

    removed string return type, decrypt should work but doesn't

--- ecb.py
+++ ecb.py
@@ -1,18 +1,22 @@
 import core
 import base64
+import itertools
+import struct
 
 def encrypt(message, key, b64=False):
     # convert message to 128-bit blocks
     blocks = core.to_blocks(map(ord, message))
+
     # transform each block and apply transformation from array to matrix
     for block in blocks:
         core.transform(block)
         core.encrypt_block(block, key)
         core.transform(block)
 
-    # flatten out blocks and join to string
-    result = ''.join([ chr(c) for c in list(*blocks) ])
-    return result if not b64 else base64.b64encode(result)
+    # flatten out blocks
+    flat = list(itertools.chain.from_iterable(blocks))
+    print flat
+    return flat
 
 def encrypt_file(file, key, b64=False):
     with open(file, 'r') as fr:
@@ -21,14 +25,15 @@ def encrypt_file(file, key, b64=False):
             fw.write(message)
 
 def decrypt(message, key, b64=False):
-    blocks = core.to_blocks(map(ord, message))
+    blocks = core.to_blocks(message)
     for block in blocks:
         core.transform(block)
         core.decrypt_block(block, key)
         core.transform(block)
 
-    result = ''.join([ chr(c) for c in list(*blocks) ])
-    return result if not b64 else base64.b64decode(result)
+    flat = list(itertools.chain.from_iterable(blocks))
+    print flat
+    return flat
 
 def decrypt_file(file, key, b64=False):
     with open(file, 'r') as fr:
